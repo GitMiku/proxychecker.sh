cabc808ca58e3793205af8fb9e83a810b411ebec
GitMiku <MikuMiku@lavabit.com>
2014-05-12 04:47:40 +0200

    Create proxychecker.sh

---
+++ proxychecker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#Filename: proxycheck.sh
+#Purpose: Simple Proxy Checker
+
+echo "Welcome to $0, a simple proxy checker"
+echo "Enter the name of the proxy list file to use"
+echo "The proxies should be in IP:PORT format"
+read list
+resultfile=goodproxies$(date "+%m%d%y%H%M%S").txt
+failurefile=badproxies$(date "+%m%d%y%H%M%S").txt
+touch $resultfile
+touch $failurefile
+
+echo "Checking proxies from the list file $list"
+for i in $(cat $list)
+do
+    results=$(curl -fsx $i google.com)
+    if  [ -z "$results" ]
+    then
+        echo $i >> $failurefile
+    else
+        echo $i >> $resultfile
+    fi
+done
+
+echo "Finished after $SECONDS seconds"
+echo "Good proxies written to $resultfile"
+echo "Bad proxies written to $failurefile"
+
+echo "Do you want to open the good proxies list file?"
+select yn in "Yes" "No"
+do
+    case $yn in
+        Yes ) xdg-open $resultfile; break;;
+        No ) exit;;
+    esac
+done
